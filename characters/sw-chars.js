c87670a21b9e9b75715d0525ef52604bcdfd9922
dai <89108799+dandaifuku@users.noreply.github.com>
2021-10-26 12:44:26 -0600

    added buttons and fixed films

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,19 +1,54 @@
 import { people } from '../data/people.js'
+import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('#main')
 
+const mainHeader = document.createElement('header')
+document.body.insertBefore(mainHeader, main)
+
+const allButton = document.createElement('button')
+allButton.textContent = 'All Characters'
+
+allButton.addEventListener('click', () => populateDOM(people))
+
+mainHeader.appendChild(allButton)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+mainHeader.appendChild(femaleButton)
+
+const othersButton = document.createElement('button')
+othersButton.textContent = 'Other Characters'
+
+othersButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+mainHeader.appendChild(othersButton)
+
 const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters.length)
 const femaleCharacters = people.filter(person => person.gender === 'female')
-console.log(femaleCharacters.length)
 
 const otherCharacters = people.filter(person => {
-    if (person.gender === 'n/a' || person.gender === 'hermaphrodite') {
+    if (person.gender === 'n/a' || person.gender === 'hermaphrodite' || person.gender === 'none') {
         return person
     }
 })
 
-people.forEach((element) => {
+function populateDOM(characters) {
+    // remove all the previous items before populating with new ones
+    while (main.firstChild) {
+        main.removeChild(main.firstChild)
+    }
+characters.forEach((element) => {
 const personFig = document.createElement('figure')
 const personImg = document.createElement('img')
 let charNum = getLastNumber(element.url)
@@ -26,12 +61,4 @@ personFig.appendChild(personCaption)
 
 main.appendChild(personFig)
 })
-
-function getLastNumber(url) {
- let end = (url.lastIndexOf('/'))
- let start = end - 2
- if (url.charAt(start) === '/') {
-    start++
- }
- return (url.slice(start, end))
 }

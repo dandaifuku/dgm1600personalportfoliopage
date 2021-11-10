d7f2dc18a91ec27fc42d948a6d5c8f1d22d865a1
dai <89108799+dandaifuku@users.noreply.github.com>
2021-11-09 17:06:11 -0700

    congress page

---
+++ congress/congress.js
@@ -0,0 +1,45 @@
+import { senators } from '../data/senators.js'
+
+const senatorDiv = document.querySelector('.senators')
+
+function simplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` ` 
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            party: senator.party,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+            gender: senator.gender,
+            seniority: "later",
+            missedVotesPct: senator.missed_votes_pct,
+            loyaltyPct: senator.votes_with_party_pct,
+        }
+    })
+}
+
+populateSenatorDiv(simplifiedSenators(senators))
+
+function populateSenatorDiv(simpleSenators) {
+    simpleSenators.forEach(senator => {
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+
+        figCaption.textContent = senator.name
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senatorDiv.appendChild(senFigure)
+    })
+}
+
+const filterSenators = (prop, value) => {
+    return simplifiedSenators(senators).filter(senator => senator[prop] === value)
+}
+
+const republicans = filterSenators('party', 'R')
+const femaleSenators = filterSenators('gender', 'F')
+
+console.log(femaleSenators)
